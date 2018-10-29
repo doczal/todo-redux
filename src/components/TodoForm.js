7d4f52450ff34797c67c236733ca48b3d9eec509
Rizal <doczal@gmail.com>
2018-10-29 22:13:09 +0800

    Initial Commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './TodoForm.css';
+
+class TodoForm extends Component {
+  
+}
+
+export default TodoForm;
---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default TodoList;
