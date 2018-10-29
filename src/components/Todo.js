3be4599ec04a0e647d657fd8332a653e536db8f7
Rizal <doczal@gmail.com>
2018-10-29 23:24:40 +0800

    Added delete button and more styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,8 +3,11 @@ import './Todo.css';
 
 const Todo = ({ task, complete }) => (
   <li className={`todo ${complete ? 'complete' : ''}`}>
-    <div className="todo-status"></div>
-    { task }
+    <div className="todo-content">
+      <div className="todo-status"></div>
+      { task }
+    </div>
+    <button className="todo-del">DEL</button>
   </li>
 );
 
